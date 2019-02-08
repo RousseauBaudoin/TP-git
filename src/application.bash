ea9c8f4ccb1988dc4344d3a1864bfa8bf85fbb6d
Baudoin Rousseau <Baudoin.rousseau@gmail.com>
2019-02-08 10:59:29 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,6 @@
-### Application.bash - 2018 - ROUSSEAU ###
-echo "Copyright Company - 2018"
-echo "Build : 10:00"
-echo "version 1.1"
+### Application.bash - 2019 - ROUSSEAU ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
